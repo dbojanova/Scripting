e7aec559c4ef0f354483711e84a56fd6e74de5c6
Diana (Didi) Bojanova <dbojanova@users.noreply.github.com>
2024-01-13 16:36:14 -0800

    Update remove_seqs.py

--- remove_seqs.py
+++ remove_seqs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 from Bio import SeqIO
-import sys
 import argparse
+import sys
 
 #help file that deliniates required arguments
 parser = argparse.ArgumentParser(description='Enter a list of headers to remove and a fasta file to remove them from.')
@@ -11,22 +11,17 @@ parser.add_argument('--input', required=True, help='file of fasta sequences')
 parser.add_argument('--out', required=True, help='file of results')
 args=parser.parse_args()
 
-#print warning if not enough arguments
-if len(sys.argv) < 3: 
-  print("\nYou need to provide: \n(1) a list of headers to remove\n(2) a fasta file to remove them from\n(3) output file.")
+#print warning if no arguments given
+if not len(sys.argv) > 1: 
+  parser.print_help()
   sys.exit(0)
-
-# print warning if too many arguments.
-if len(sys.argv) > 4: 
-  print("\nExtra arguments input!")
-  sys.exit(0)
+return args
 
 #identify each of the headers
-
-headers = set(line.strip() for line in open(sys.argv[2]))
+headers = set(line.strip() for line in open(args.headers))
 
 #parse through all the sequences and remove any with a header matching the inputted headers to be removed
-for seqs in SeqIO.parse(sys.argv[1], "fasta"):
+for seqs in SeqIO.parse(args.input, "fasta"):
   if seqs.id not in headers:
     SeqIO.write(seqs, args.out, "fasta")
   try:
@@ -37,4 +32,4 @@ for seqs in SeqIO.parse(sys.argv[1], "fasta"):
 
 #report any headers that were not found in the fasta file and therefore not removed
 if len(headers) != 0:
-  print(len(headers),'of the headers from list were not identified in the input fasta file.', file=sys.stderr)
+  print(len(headers),'of the headers were not in the fasta file.', file=sys.stderr)
